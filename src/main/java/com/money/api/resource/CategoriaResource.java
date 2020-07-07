3f98eec2762a45a8754d70c9500edacefd414bf5
wenner <wennersannerma@gmail.com>
2020-07-07 09:57:51 -0300

    #05 - Validando criação da categoria

--- src/main/java/com/money/api/resource/CategoriaResource.java
+++ src/main/java/com/money/api/resource/CategoriaResource.java
@@ -3,7 +3,6 @@ package com.money.api.resource;
 import com.money.api.model.Categoria;
 import com.money.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -11,6 +10,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -35,6 +35,12 @@ public class CategoriaResource {
 
         //retorna a uri e status code
         return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    @ResponseStatus
+    public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+        return categoriaRepository.findById(codigo);
     }
 
 }
