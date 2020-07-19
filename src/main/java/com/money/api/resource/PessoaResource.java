91468d3f6da1f934c3e2d1ca970f61ef42c62b1c
wenner <wennersannerma@gmail.com>
2020-07-18 22:58:14 -0300

    #15 - Atualização parcial da propriedade ativo de pessoa

--- src/main/java/com/money/api/resource/PessoaResource.java
+++ src/main/java/com/money/api/resource/PessoaResource.java
@@ -67,4 +67,11 @@ public class PessoaResource {
         return ResponseEntity.ok(pessoaSalva);
     }
 
+    @PutMapping("/{codigo}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+        this.pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+    }
+
+
 }
